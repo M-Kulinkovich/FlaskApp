e015ea90b1299d9a7dcb681f38c1b895979cbb82
M-Kulinkovich <50986910+M-Kulinkovich@users.noreply.github.com>
2023-02-19 21:23:37 +0300

    Update db.py

--- venv/db.py
+++ venv/db.py
@@ -3,7 +3,7 @@ import pymongo
 from pymongo import MongoClient
 from __init__ import app
 
-CONNECTION_STRING = "mongodb+srv://user:userzx@testdata.mz0orda.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://name:pass@namedb.mz0orda.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('testdata')
 user_collection = pymongo.collection.Collection(db, 'user_collection')
